e10059fce49e61a839968a08ca8d8256ca4b2e40
consp <none@yourbusiness.com>
2021-12-26 12:48:37 +0100

    Fixed submodule init

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
+ORIGIN=$(pwd)
 git submodule init
 git submodule update
-ln -s ./submodules/mightycore/avr/boards.txt ./atmega1284p/board/
-ln -s ./submodules/mightycore/avr/avrdude.conf ./atmega1284p/board/
-ln -s ./submodules/mightycore/avr/variants/standard/pins_arduino.h ./atmega1284p/board/
-ln -s ./submodules/mightycore/avr/cores/MCUdude_corefiles/* ./atmega1284p/cores/
-ln -s ./submodules/mightycore/avr/libraries/* ./atmega1284p/libraries/
+mkdir -p ./atmega1284p/board
+mkdir -p ./atmega1284p/cores
+mkdir -p ./atmega1284p/libraries
+ln -s ../../submodules/mightycore/avr/boards.txt ./atmega1284p/board/
+ln -s ../../submodules/mightycore/avr/avrdude.conf ./atmega1284p/board/
+ln -s ../../submodules/mightycore/avr/variants/standard/pins_arduino.h ./atmega1284p/board/
+cd $ORIGIN/atmega1284p/cores
+ln -s ../../submodules/mightycore/avr/cores/MCUdude_corefiles/* ./
+cd $ORIGIN/atmega1284p/libraries
+ln -s ../../submodules/mightycore/avr/libraries/* ./
