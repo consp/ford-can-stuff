736ca28eabd20624bb1021b422928892d1664845
consp <none@yourbusiness.com>
2022-01-20 20:46:30 +0100

    Added contribution for rewrite to pure C for library (header)

--- atmega1284p/include/mcp2515/mcp_can.h
+++ atmega1284p/include/mcp2515/mcp_can.h
@@ -1,15 +1,19 @@
 /*
   mcp_can.h
-  2012 Copyright (MCPCAN *state, c) Seeed Technology Inc.  All right reserved.
-  2017 Copyright (MCPCAN *state, c) Cory J. Fowler  All Rights Reserved.
+  2012 Copyright (c) Seeed Technology Inc.  All right reserved.
+  2017 Copyright (c) Cory J. Fowler  All Rights Reserved.
+  2021 Copyright (c) Tristan Timmermans All Rights Reserved.
 
-  Author:Loovee
+  Author: Loovee
   Contributor: Cory J. Fowler
   2017-09-25
+  Contributor: Tristan Timmermans
+  2021-01-20
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (MCPCAN *state, at your option) any later version.
+  version 2.1 of the License, or (at your option) any later version.
 
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
